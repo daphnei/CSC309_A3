099abb42f43409c452ff560354d62174708660e6
Gabriel B. Nunes <alchemicvisions@gmail.com>
2013-03-23 15:32:43 -0400

    Added helper function to insert a set of substrings into a string at a set of given indices

--- js/helper.js
+++ js/helper.js
@@ -16,5 +16,28 @@ var helper = {
      */
     insert: function(string, insertion, index) {
         return string.slice(0, index) + insertion + string.slice(index, string.length);
+    },
+    
+    /**
+     * Insert a set of strings into another string, at a set of specified indices.
+     *
+     * @param string The string which you want to insert into.
+     * @param insertions An array of objects of the form:
+     *        {text : Text you want to insert, index: Index you want to insert it at}
+     *        All indices should be positive.
+     *
+     * @returns A copy of the original string with the desired insertions.
+     */
+    insertMultiple: function(string, insertions) {
+        // Sort by index of the insertions descending order. 
+        insertions = insertions.sort(function(a, b) {return b.index - a.index;});
+        // This ensures that we insert from the end of the string first, which prevents
+        // us from having to adjust the indices as we insert things.
+        
+        for (var i=0; i < insertions.length; i++) {
+            string = this.insert(string, insertions[i].text, insertions[i].index);
+        }
+        
+        return string
     }
 }
