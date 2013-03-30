4570520eb23f4e9361058b8cc54b6de7bbc7d008
Gabriel B. Nunes <alchemicvisions@gmail.com>
2013-03-29 23:46:02 -0400

    Refactored our popup details dialog to not use jQuery grids and instead rely on manually positioned divs. Also made it nice and responsive.

--- js/generateDetailsView.js
+++ js/generateDetailsView.js
@@ -6,8 +6,7 @@ var generateDetailsView = {
 
     /**
      * Generates HTML for the header of the details dialog.
-     * <div class="ui-block-a">Block A</div>
-                    <div class="ui-block-b">Block B</div>
+     * 
      * @param tweet The tweet that you're getting the details for (?).
      */
     generateDetailsHeader: function(tweet) {
@@ -22,16 +21,14 @@ var generateDetailsView = {
      */
     generateDetailsContent: function(tweet) {
         var shtml = "";
-        shtml += "<div class='ui-block-a'>";
-        shtml += "<img src='" + tweet.user.image + "'/>";
-        shtml += "</div>";
+        shtml += "<div class='detail-user'>";
+        shtml += "<div class='avatar'><img src='" + tweet.user.image + "'/></div>";
+        
+        shtml += "<div class='name'><p>" + tweet.user.name + "<br />";
+        shtml += "<span class='usertag'>@" + tweet.user.screen_name + "</span></p></div>";
+        shtml += "<div class='clear-both'></div></div>";
 
-        shtml += "<div class='ui-block-b name-detail'>";
-        shtml += "<p class='name'>" + tweet.user.name + "<br />";
-        shtml += "<span class='usertag'>@" + tweet.user.screen_name + "</span></p>";
-        shtml += "</div>";
-
-        shtml += "<div class='ui-block-c'>";
+        shtml += "<div class='detail-text'>";
         shtml += "<p>" + tweet.user.description + "</p>";
 
         //format the location and website pretty with a dot between them like Twitter does
@@ -47,6 +44,7 @@ var generateDetailsView = {
         }
         
         shtml += "</div>";
+        shtml += "<div class='clear-both'></div>";
         return shtml;
     }
 };
